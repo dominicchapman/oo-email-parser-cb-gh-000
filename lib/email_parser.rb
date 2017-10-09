0b5f7642fdd1b4bd952707f95a43da5677535748
Dominic Chapman <dominicchapman@me.com>
2017-10-09 21:59:57 +0000

    automatically backed up by learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -4,15 +4,15 @@
 # or whitespace (' ').
 
 class EmailParser
-  
+
   attr_accessor :emails
-  
+
   def initialize(emails)
     @emails = emails
   end
-  
+
   def parse
     @emails.split(/[\s,]+/)
   end
-  
+
 end
